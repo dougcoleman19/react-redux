ac65f86887d72f2b5f721b396e23b3825a948b4b
Doug Coleman <douglas.coleman@daugherty.com>
2018-07-25 16:26:11 -0500

    More from the tutorial

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,3 +1,21 @@
-export function createCourse(course) {
-    return { type: 'CREATE_COURSE', course};
+import * as types from './actionTypes';
+import courseApi from '../api/mockCoursesApi';
+
+
+export function loadCoursesSuccess(courses) {
+    return { type: types.LOAD_COURSES_SUCCESS, courses};
+}
+
+// THUNKS
+
+export function loadCourses() {
+    return function(dispatch) {
+        return courseApi.getAllCourses()
+            .then(courses => {
+                dispatch(loadCoursesSuccess(courses));
+            })
+            .catch(error => {
+                throw error;
+            });
+    };
 }
